c7669a16fdffb437edf322699e19cc1073dfba33
Hema Veeraragavathatham <hema.ragavan@gmail.com>
2024-06-04 11:24:27 -0700

    Adjust font sizes

--- src/components/AllCategories.js
+++ src/components/AllCategories.js
@@ -12,10 +12,10 @@ const Home = () => {
     <>
       <Navbar />
       <div className="h-full w-full flex flex-col p-10">
-        <div className="rounded-md p-5 bg-black align-middle text-center xs:text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl bold tracking-wide text-gray-100 ">
+        <div className="rounded-md p-5 bg-black align-middle text-center xs:text-md sm:text-lg md:text-xl lg:text-3xl xl:text-4xl bold tracking-wide text-gray-100 ">
           Welcome!
         </div>
-        <div className="rounded-md p-5 bg-black align-middle text-center xs:text-sm sm:text-md md:text-lg lg:text-xl xl:text-2xl bold tracking-wide text-gray-100 ">
+        <div className="rounded-md p-5 bg-black align-middle text-center xs:text-sm sm:text-md md:text-lg lg:text-2xl xl:text-3xl bold tracking-wide text-gray-100 ">
           Enjoy taking a musical journey to the Vaishnava Divya Desams alongside{" "}
           <br />
           Dr. D. Veeraragavathatham
@@ -41,7 +41,7 @@ const Home = () => {
                   >
                     <div className="flex h-full items-center justify-center">
                       <div className="text-white">
-                        <h1 className="text-center xs:text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl bold tracking-wide text-white ">
+                        <h1 className="text-center xs:text-md sm:text-lg md:text-2xl lg:text-3xl xl:text-4xl bold tracking-wide text-white ">
                           {category}
                         </h1>
                       </div>
